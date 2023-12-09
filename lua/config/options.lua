3649fdd471418666b7a17485986121aeab4f39f3
westerntoad <abr.engebretson@gmail.com>
2023-12-09 02:02:07 -0800

    indent options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,6 +3,12 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 
+-- indent and tab settings
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.tabstop = 8
+
+-- line numbering
 vim.opt.cursorline = true
 vim.opt.number = true
 vim.opt.relativenumber = false
